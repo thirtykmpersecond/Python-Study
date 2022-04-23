fed96a4e7ab61ca86f830856646f58ed19c2e3b5
xzw <thirtykmpersecond@icloud.com>
2022-04-23 19:15:31 +0800

    学习到数据导出

--- Python语言基础/Pandas包/数据导出.py
+++ Python语言基础/Pandas包/数据导出.py
@@ -1,8 +1,11 @@
 from pandas import DataFrame
 from pandas import Series
-df = DataFrame({'age':Series([26,85,64]),
-                'name':Series(['Ben','John','Jerry'])
-                })
-print(df)
-df.to_csv('~/Desktop/01.csv')
-df.to_csv('~/Desktop/02.csv',index=False)
+
+df = DataFrame(
+    {
+        'age':Series([26,85,64]),
+        'name':Series(["Ben","John","Jerry"])
+    }
+)
+df.to_excel('d:\\01.xlsx')
+df.to_excel('d:\\02.xlsx',index=False)
