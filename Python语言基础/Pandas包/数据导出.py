ca7e19ce0b0a19a6d3f57d60349f5eed6a5d7237
xzw <thirtykmpersecond@icloud.com>
2022-04-24 01:32:49 +0800

    学习到数MySQL

--- Python语言基础/Pandas包/数据导出.py
+++ Python语言基础/Pandas包/数据导出.py
@@ -1,4 +1,9 @@
 from pandas import DataFrame
 from pandas import Series
 
-df =
+df = DataFrame({'age':Series([26,85,64]),
+                'name':Series(['Ben','John','Jerry'])
+                })
+
+df.to_excel('~/Desktop/01.xlsx')
+df.to_excel('~/Desktop/02.xlsx')
