0b97db8cc8d2b51ae1294a05e0d2a3b63c07e70e
xzw <thirtykmpersecond@icloud.com>
2022-04-24 19:24:57 +0800

    学习到输出MySQL

--- Python语言基础/Pandas包/数据导出.py
+++ Python语言基础/Pandas包/数据导出.py
@@ -1,9 +1,18 @@
 from pandas import DataFrame
 from pandas import Series
+from sqlalchemy import create_engine
+#import pymysql
+#启动引擎
+#此处一定写为mysql+pymysql
+engine = create_engine("mysql+pymysql://xzw:Hasmanafuture?@127.0.0.1:3306/mytest?charset=utf8")
+#
+df = DataFrame({'app_name':Series(['QQ 音乐','京东 APP']),
+                'url':Series(['http://music.qq.com','http://jd.com']),
+                'country':Series(['CN','CN'])})
 
-df = DataFrame({'age':Series([26,85,64]),
-                'name':Series(['Ben','John','Jerry'])
-                })
-
-df.to_excel('~/Desktop/01.xlsx')
-df.to_excel('~/Desktop/02.xlsx')
+#存放入MySQL
+df.to_sql(name='apps',
+          con=engine,
+          if_exists='append',
+          index=False,
+          index_label=False)
